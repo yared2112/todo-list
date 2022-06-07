43e5ef247c2165fdf0d086c8cb7cb9dd8e7a4fd6
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 12:33:31 +0300

    completed task

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -4,6 +4,17 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const [todos, setTodos] = useState([])
 
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todos, ...todos]
+
+        setTodos(newTodos)
+        console.log(...todos)
+    }
+
   return (
     <div>
         <h1>What is the plan for today</h1>
