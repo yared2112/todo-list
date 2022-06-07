a3c8277d721d234c5b1804747f27cc12bf163ebf
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 13:40:40 +0300

    completed task

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,51 +1,57 @@
-import React, {useState} from 'react'
-import Todo from './Todo'
-import TodoForm from './TodoForm'
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return
-        }
-
-        const newTodos = [todos, ...todos]
-
-        setTodos(newTodos)
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
 
-    const updateTodo = (todoId, newValue) => {
-        if (!newValue.text || /^\s*$/.test(newValue.text)) {
-          return
-        }
-    
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
-      }
+    const newTodos = [todo, ...todos];
 
-    const removeTodo = id => {
-        const removedArr = [...todos].filter(todo => todo.id !== id)
-    
-        setTodos(removedArr)
-      }
+    setTodos(newTodos);
+    console.log(...todos);
+  };
 
-    const completeTodo = id => {
-        let updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete;
-            }
-            return todo;
-        })
-        setTodos(updatedTodos)
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
     }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div>
-        <h1>What is the plan for today</h1>
-        <TodoForm onSubmit={addTodo} />
-        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
-    </div>
-  )
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
 }
 
-export default TodoList
+export default TodoList;
