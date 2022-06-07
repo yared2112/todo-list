73ecc9f97d11ecff31b167a1f348cec31ae9e386
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 13:06:27 +0300

    completed task

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -12,13 +13,23 @@ function TodoList() {
         const newTodos = [todos, ...todos]
 
         setTodos(newTodos)
-        console.log(todo, ...todos)
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos)
     }
 
   return (
     <div>
         <h1>What is the plan for today</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo}/>
     </div>
   )
 }
