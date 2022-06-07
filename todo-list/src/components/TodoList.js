321f7c78cf8160f9f14f702336af4a527b088a69
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 13:19:47 +0300

    completed task

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -15,6 +15,14 @@ function TodoList() {
         setTodos(newTodos)
     }
 
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+          return
+        }
+    
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+      }
+
     const removeTodo = id => {
         const removedArr = [...todos].filter(todo => todo.id !== id)
     
@@ -35,7 +43,7 @@ function TodoList() {
     <div>
         <h1>What is the plan for today</h1>
         <TodoForm onSubmit={addTodo} />
-        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   )
 }
