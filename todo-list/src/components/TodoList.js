18a95eba23efb314e91ca404319454c87b0a56e3
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 12:28:02 +0300

    completed task

---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+    const [todos, setTodos] = useState([])
+
+  return (
+    <div>
+        <h1>What is the plan for today</h1>
+        <TodoForm />
+    </div>
+  )
+}
+
+export default TodoList
